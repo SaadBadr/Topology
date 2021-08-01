35ff889df664d61ab9a841c68033f7230fbdd3d4
SaadBadr <saadbadr23@gmail.com>
2021-08-01 17:09:14 +0200

    removing unnecessary lines from topology.js

--- topology.js
+++ topology.js
@@ -1,93 +1,83 @@
-const fs = require('fs');
-const { promisify } = require('util');
-
-class Topology {
-    constructor(id, components) {
-        this.id = id;
-        this.components = components;
-    }
-};
+const fs = require("fs");
 
 class Topologies {
+  constructor() {
+    this.currentTopologies = [];
+  }
 
-    constructor() {
-        this.currentTopologies = [];
+  readJSON(fileName) {
+    try {
+      const topology = fs.readFileSync(fileName);
+      const topologyOBJ = JSON.parse(topology);
+      this.currentTopologies.push(topologyOBJ);
+      console.log("Topology read success");
+    } catch (error) {
+      console.log("Topology read failed");
+      console.log(error);
+    }
+  }
+
+  writeJSON(TopologyID) {
+    const topology = this.currentTopologies.find((t) => t.id === TopologyID);
+
+    if (!topology) {
+      console.log("this topology doesn't exist!");
+      return;
     }
 
-    readJSON(fileName) {
-        try {
-            const topology = fs.readFileSync(fileName);
-            const topologyOBJ = JSON.parse(topology);
-            this.currentTopologies.push(topologyOBJ);
-            console.log("Topology read success");
-    
-        } catch (error) {
-            console.log("Topology read failed");
-            console.log(error);
-        }        
+    try {
+      fs.writeFileSync("outpus.json", JSON.stringify(topology));
+      console.log("Topology write success");
+    } catch (error) {
+      console.log("Topology write failed");
+      console.log(error);
+    }
+  }
+
+  queryTopologies() {
+    return this.currentTopologies.map((t) => t.id);
+  }
+
+  deleteTopology(TopologyID) {
+    this.currentTopologies = this.currentTopologies.filter(
+      (t) => t.id != TopologyID
+    );
+  }
+
+  queryDevices(TopologyID) {
+    const topology = this.currentTopologies.find((t) => t.id === TopologyID);
+
+    if (!topology) {
+      console.log("this topology doesn't exist!");
+      return;
     }
 
-    writeJSON(TopologyID) {
-        const topology = this.currentTopologies.find(t => t.id === TopologyID)
-        
-        if(!topology)
-        {
-            console.log("this topology doesn't exist!");
-            return;
-        }
-        
-        try {
-            fs.writeFileSync("outpus.json", JSON.stringify(topology))
-            console.log("Topology write success");
-    
-        } catch (error) {
-            console.log("Topology write failed");
-            console.log(error);   
-        }
+    return topology.components.map((c) => c.id);
+  }
 
+  queryDevicesWithNetlistNode(TopologyID, NetlistNodeID) {
+    const topology = this.currentTopologies.find((t) => t.id === TopologyID);
+
+    if (!topology) {
+      console.log("this topology doesn't exist!");
+      return;
     }
 
-    queryTopologies() {
-        return this.currentTopologies.map(t => t.id)
-    }
+    const devices = topology.components.reduce((result, component) => {
+      const idx = Object.values(component.netlist).findIndex(
+        (n) => n == NetlistNodeID
+      );
+      if (idx !== -1)
+        result.push({
+          id: component.id,
+          terminal: Object.keys(component.netlist)[idx],
+        });
 
-    deleteTopology(TopologyID) {
-        this.currentTopologies = this.currentTopologies.filter(t => t.id != TopologyID)
-    }
+      return result;
+    }, []);
 
-    queryDevices(TopologyID) {
-        const topology = this.currentTopologies.find(t => t.id === TopologyID)
-      
-        if(!topology)
-        {
-            console.log("this topology doesn't exist!");
-            return;
-        }
-      
-        return topology.components.map(c => c.id)
-    }
-
-    queryDevicesWithNetlistNode(TopologyID, NetlistNodeID) {
-        const topology = this.currentTopologies.find(t => t.id === TopologyID)
-      
-        if(!topology)
-        {
-            console.log("this topology doesn't exist!");
-            return;
-        }
-        
-        const devices = topology.components.reduce((result, component) => {
-            const idx = Object.values(component.netlist).findIndex(n => n == NetlistNodeID)
-            if(idx !== -1)
-                result.push({id: component.id, terminal: Object.keys(component.netlist)[idx]})
-
-            return result
-        }, [])
-
-        return devices
-
-    }
+    return devices;
+  }
 }
 
-
-module.exports = new Topologies()
+module.exports = new Topologies();
